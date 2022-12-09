81cc1faa31d25d6302f9e30f7f4d4fd6c778f821
mcdowell84 <68907957+mcdowell84@users.noreply.github.com>
2022-12-09 23:26:04 +0100

    not working 03B

---
+++ 03B.py
@@ -0,0 +1,38 @@
+sum = 0
+
+def splitmonkey(input):
+    string1 = input[0].strip()
+    string2 = input[1].strip()
+    string3 = input[2].strip()
+
+    return string1, string2, string3
+
+def checkmonkey(string1, string2, string3):
+    result = set(string1).intersection(string2, string3)
+    char = result.pop()
+    return char
+
+def countmonkey(char):
+    if char.islower() == True:
+        number = ord(char) - 96
+    else:
+        number = ord(char) - 64 + 26
+    return number
+
+chunked_list = list()
+chunk_size = 3
+
+with open("input/03test.txt") as file:
+    sample = file.readlines()
+
+    for i in range(0, len(sample), chunk_size):
+        chunked_list.append (sample[i:i+chunk_size])
+
+for count, value in enumerate(chunked_list):
+    set = splitmonkey(value)
+
+    char = checkmonkey(set[0], set[1], set[2])
+    number = countmonkey(char)
+    sum += number
+
+print(sum)
